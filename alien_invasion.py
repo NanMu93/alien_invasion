ecd295273fb4b6c16934dee15820c5bca9b66008
zhongyw <mscboom@gmail.com>
2022-05-13 17:38:06 +0800

    同步

---
+++ alien_invasion.py
@@ -0,0 +1,97 @@
+import sys
+import pygame
+from settings import Settings
+from ship import Ship
+from bullet import Bullet
+
+
+class AlienInvasion:
+    """管理游戏资源和行为的类"""
+
+    def __init__(self):
+        """初始化游戏并创建游戏资源"""
+        pygame.init()
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+#        在全屏模式下运行
+#        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
+
+        pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
+
+    def run_game(self):
+        """开始游戏的主循环"""
+        while True:
+            # 监视键盘和鼠标事件
+            self._check_events()
+            self.ship.update()
+            self.bullets.update()
+
+            # 删除消失的子弹
+            for bullet in self.bullets.copy():
+                if bullet.rect.bottom <= 0:
+                    self.bullets.remove(bullet)
+            print(len(self.bullets))
+
+            self._update_screen()
+
+    def _check_events(self):
+        """响应按键和鼠标事件"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_event(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_event(self, event):
+        if event.key == pygame.K_q:
+            sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
+        if event.key == pygame.K_RIGHT:
+            # 向右移动飞船
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            # 向左移动飞船
+            self.ship.moving_left = True
+        if event.key == pygame.K_UP:
+            # 向上移动飞船
+            self.ship.moving_up = True
+        elif event.key == pygame.K_DOWN:
+            # 向下移动飞船
+            self.ship.moving_down = True
+
+    def _check_keyup_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+        if event.key == pygame.K_UP:
+            self.ship.moving_up = False
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = False
+
+    def _fire_bullet(self):
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+
+    def _update_screen(self):
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
+
+        # 让最近绘制的屏幕可见
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    ai = AlienInvasion()
+    ai.run_game()
